61c30d1bebc1fe6def2f03eedb0e1bb8abfcc6cf
munnaMia <mdmunnamia987@gmail.com>
2026-01-26 23:19:07 +0600

    add: grayscalse

--- cmd/run.go
+++ cmd/run.go
@@ -11,6 +11,8 @@ import (
 )
 
 func Run() {
+	var asciiImage []byte
+
 	filePath := flag.String("p", `./images/tjImg_logo.png`, "image file path")
 	flag.Parse()
 
@@ -32,8 +34,15 @@ func Run() {
 	fmt.Println("Red Green Blue Alpha")
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			r, g, b, a := img.At(x, y).RGBA()
-			fmt.Printf("Color code : %v %v %v %v\n", r, g, b, a)
+			r, g, b, _ := img.At(x, y).RGBA()
+			r, g, b = r>>8, g>>8, b>>8 // convert 16 bit to 8 bit
+
+			grayScale := int((0.299 * float64(r)) + (0.587 * float64(g)) + (0.114 * float64(b)))
+			asciiImage = append(asciiImage, byte(grayScale))
+			fmt.Printf("Color code : %v %v %v %v\n", r, g, b, grayScale)
 		}
+		asciiImage = append(asciiImage, '\n')
 	}
+
+	fmt.Println(asciiImage)
 }
