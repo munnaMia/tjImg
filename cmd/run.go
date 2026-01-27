da76bc6ef3ad85518b3496257b392cd2e47534f8
munnaMia <mdmunnamia987@gmail.com>
2026-01-27 22:46:42 +0600

    fix: 2d matrix array of gray values

--- cmd/run.go
+++ cmd/run.go
@@ -11,10 +11,9 @@ import (
 )
 
 func Run() {
-	var grayImageCodes []byte
 
 	filePath := flag.String("p", `./images/tjImg_logo.png`, "image file path")
-	scale := flag.Int("x", 10, "scale down the image x time")
+	// scale := flag.Int("x", 10, "scale down the image x time")
 	flag.Parse()
 
 	file, err := os.Open(*filePath)
@@ -35,42 +34,45 @@ func Run() {
 	xMin, xMax := bounds.Min.X, bounds.Max.X
 	yMin, yMax := bounds.Min.Y, bounds.Max.Y
 
-	imageWidth := xMax - xMin
-	imageHeight := yMax - yMin
+	// imageWidth := xMax - xMin
+	// imageHeight := yMax - yMin
+	var grayImageCodes [][]byte
 
 	for y := yMin; y < yMax; y++ {
+		var rowGrayCodes []byte
 		for x := xMin; x < xMax; x++ {
 			r, g, b, _ := img.At(x, y).RGBA()
 			r, g, b = r>>8, g>>8, b>>8 // convert 16 bit to 8 bit
 
 			grayScale := int((0.299 * float64(r)) + (0.587 * float64(g)) + (0.114 * float64(b)))
-			grayImageCodes = append(grayImageCodes, byte(grayScale))
+			// grayImageCodes = append(grayImageCodes, byte(grayScale))
+			rowGrayCodes = append(rowGrayCodes, byte(grayScale))
 			fmt.Printf("Color code : %v %v %v %v\n", r, g, b, grayScale)
 		}
-		grayImageCodes = append(grayImageCodes, byte('\n'))
+		grayImageCodes = append(grayImageCodes, rowGrayCodes)
 	}
 
-	colGroup := imageWidth / *scale
-	rowGroup := imageHeight / *scale
+	// colGroup := imageWidth / *scale
+	// rowGroup := imageHeight / *scale
 
-	var rowCompressGrayimageCode []byte
+	// var rowCompressGrayimageCode []byte
 
-	for row := range rowGroup {
-		for col := range colGroup {
-			grayValueSum := 0
-			for s := range *scale {
-				idx := row*col**scale + s
+	// for row := range rowGroup {
+	// 	for col := range colGroup {
+	// 		grayValueSum := 0
+	// 		for s := range *scale {
+	// 			idx := row*col**scale + s
 
-				fmt.Println("test", idx)
-				if grayImageCodes[idx] == byte('\n') { // 10 mean \n new line
-					break
-				}
-				grayValueSum += int(grayImageCodes[idx])
-				rowCompressGrayimageCode = append(rowCompressGrayimageCode, byte(grayValueSum / *scale))
-			}
-			rowCompressGrayimageCode = append(rowCompressGrayimageCode, byte(grayValueSum / *scale), byte('\n'))
-		}
-	}
-	fmt.Println(len(rowCompressGrayimageCode))
+	// 			fmt.Println("test", idx)
+	// 			if grayImageCodes[idx] == byte('\n') { // 10 mean \n new line
+	// 				break
+	// 			}
+	// 			grayValueSum += int(grayImageCodes[idx])
+	// 			rowCompressGrayimageCode = append(rowCompressGrayimageCode, byte(grayValueSum / *scale))
+	// 		}
+	// 		rowCompressGrayimageCode = append(rowCompressGrayimageCode, byte(grayValueSum / *scale), byte('\n'))
+	// 	}
+	// }
+	fmt.Println(grayImageCodes)
 
 }
